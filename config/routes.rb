81659ffe279eaa90dac0eb8d681ade4458b3ddad
Timur Meyster <timur.meyster@gmail.com>
2015-05-06 13:11:13 -0700

    added root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'documents#index'
+
   resources :users, only: [:new, :create]
   resources :documents, only: [:index, :new, :create]
   resource :session
