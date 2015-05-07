4cafb1a26114f75ddfbb47381c41fbadf29be66d
Timur Meyster <timur.meyster@gmail.com>
2015-05-07 13:35:02 -0700

    removed @doc from the views

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'app36600667@heroku.com',
+  :password => 'xjxrcibc8724',
+  :domain => 'loansapp.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
