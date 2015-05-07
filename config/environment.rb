65e21a659cd9e35e10182ad30811fc03af592c9c
Timur Meyster <timur.meyster@gmail.com>
2015-05-07 15:16:11 -0700

    trying a new approach for action mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,21 +4,31 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-if Rails.env.production?
+# if Rails.env.production?
+#
+#   ActionMailer::Base.smtp_settings = {
+#     :address        => 'smtp.sendgrid.net',
+#     :port           => '587',
+#     :authentication => :plain,
+#     :user_name      => ENV['pusher_app_USER_NAME'],
+#     :password       => ENV['pusher_app_SdG_PASSWORD'],
+#     :domain         => 'heroku.com',
+#     :enable_starttls_auto => true
+#   }
+#
+#   ActionMailer::Base.delivery_method ||= :smtp
+#
+# elsif Rails.env.development?
+#
+#   ActionMailer::Base.delivery_method = :letter_opener
+# end
 
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['pusher_app_USER_NAME'],
-    :password       => ENV['pusher_app_SdG_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
-
-  ActionMailer::Base.delivery_method ||= :smtp
-
-elsif Rails.env.development?
-
-  ActionMailer::Base.delivery_method = :letter_opener
-end
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['pusher_app_USER_NAME'],
+  :password       => ENV['pusher_app_SdG_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
