d1f23a6f84770548e3ff637ac4ec7721e9dccdae
Timur Meyster <timur.meyster@gmail.com>
2015-05-07 14:35:01 -0700

    added an action mailer for new docs

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -28,7 +28,7 @@ class DocumentsController < ApplicationController
   def show
     @document = Document.find(params[:id])
 
-    DocumentMailer.document_email().deliver
+    DocumentMailer.document_email(current_user, @document).deliver
 
     respond_to do |format|
       format.html
